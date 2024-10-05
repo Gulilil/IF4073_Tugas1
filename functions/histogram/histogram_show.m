452f912f085e6db58bbd56f697e3f07be9c563d4
Gulilil <juan.csantoso@gmail.com>
2024-10-05 20:55:34 +0700

    fix: histogram show

--- functions/histogram/histogram_show.m
+++ functions/histogram/histogram_show.m
@@ -1,4 +1,4 @@
-function histogram_show(histogram_data, histogram_data_r, histogram_data_g, histogram_data_b is_grayscale)
+function histogram_show(histogram_data, histogram_data_r, histogram_data_g, histogram_data_b, is_grayscale)
     if (is_grayscale)
         % Menampilkan histogram grayscale
         subplot(1, 1, 1);
@@ -6,28 +6,27 @@ function histogram_show(histogram_data, histogram_data_r, histogram_data_g, hist
         title('Grayscale Histogram');
         set(gca, 'XLim', [0 255], 'YLim', [0 max(histogram_data)]); % Set properti sumbu x dan sumbu y
     else
-        subplot(1, 1, 1);
-        bar(0:255, combined_hist, 'FaceColor', 'Magenta', 'EdgeColor', 'Magenta');  % Plot histogram
+        subplot(2, 3, 2);
+        bar(0:255, histogram_data, 'FaceColor', 'Magenta', 'EdgeColor', 'Magenta');  % Plot histogram
         title('Combined Histogram');
-        set(gca, 'XLim', [0 255], 'YLim', [0 max(combined_hist)]); % Set properti sumbu x dan sumbu y
+        set(gca, 'XLim', [0 255], 'YLim', [0 max(histogram_data)]); % Set properti sumbu x dan sumbu y
            
-        
         for i= 1:3
-            if (i == 1):
+            if (i == 1)
                 channel_hist = histogram_data_r;
-            elseif(i == 2):
+            elseif(i == 2)
                 channel_hist = histogram_data_g;
             else
                 channel_hist = histogram_data_b;
             end
-            
+
             clr = 'rgb';        % Identifikasi RGB
             clrTxt = {'Red', 'Green', 'Blue'};
             subplot(2, 3, i + 3); 
             % Plot histogram RGB
             bar(0:255, channel_hist, 'FaceColor', clr(i), 'EdgeColor', 'none');  % Plot histogram
             set(gca, 'XLim', [0 255], 'YLim', [0 max(channel_hist)]);  % Set properti sumbu
-            title([clrTxt{i}, ' Histogram (without imhist)']);
+            title([clrTxt{i}, ' Histogram']);
             xlabel('Intensity');
         end
     end
